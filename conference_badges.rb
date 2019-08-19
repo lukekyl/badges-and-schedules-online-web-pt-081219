35250f43ec6609edc50d7513257eebdb89755ee5
Kyle Luke <th.oldschool@gmail.com>
2019-08-19 21:15:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(array)
   message_array
 end
 
-def printer(attendees)
-  print_array = batch_badge_creator[attendees]
+def printer
+  print_array = batch_badge_creator
   p print_array
 end
