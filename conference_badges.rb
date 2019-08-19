fc81d25443bc0aa8ab5f25fa5ae8a411da5016b2
Kyle Luke <th.oldschool@gmail.com>
2019-08-19 21:15:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_batch = []
+  array.each{|name| badge_batch << "Hello, my name is #{name}."}
+  badge_batch
+end
+
+def assign_rooms(array)
+  room = 1
+  message_array = []
+  array.each{|name| 
+    message_array << "Hello, #{name}! You'll be assigned to room #{room}!" 
+    room += 1
+  }
+  message_array
+end
+
+def printer(attendees)
+  print_array = batch_badge_creator[attendees]
+  p print_array
+end
